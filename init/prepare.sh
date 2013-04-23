9039104fa9a0d0779322c078e37266111722c14c
stephen soltesz <stephen.soltesz@gmail.com>
2013-04-23 04:07:24 +0000

    copy customized start.sh

--- init/prepare.sh
+++ init/prepare.sh
@@ -88,6 +88,7 @@ fi
 cp -r $OONIB_PATH/scripts/init $BUILD_DIR
 rsync -ar --exclude .git $OONIB_PATH $BUILD_DIR/
 cp -r $SOURCE_DIR/init/initialize.sh $BUILD_DIR/init
+cp -r $SOURCE_DIR/init/start.sh $BUILD_DIR/init
 cp -r $SOURCE_DIR/init/stop.sh $BUILD_DIR/init
 
 # remove pre-compiled .py files

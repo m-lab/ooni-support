75ea99b4c3604aadc5f2a1d8933f3a63c743729f
Nathan Wilcox <nejucomo@gmail.com>
2014-07-17 14:38:02 -0700

    Add a setup.py.

---
+++ mlab-ns-simulator/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+VERSION = '0.0'
+
+
+setup(
+    name='mlab-ns-simulator',
+    description='A simulator for the mlab-ns service which provides features Ooni needs.',
+    version=VERSION,
+    author='LeastAuthority',
+    author_email='consultancy@leastauthority.com',
+    license='FIXME',
+    url='https://github.com/LeastAuthority/ooni-support',
+    scripts=[],
+    py_modules=[],
+    install_requires=[],
+    )

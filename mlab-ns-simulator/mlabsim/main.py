dffe7571eb5a7d96dfe77fb7a5ab081476b786d5
Nathan Wilcox <nejucomo@gmail.com>
2014-07-17 18:53:40 -0700

    Add twisted dependency and some setuptools metadata for our main function and non-existent tests.  Add a stub NotImplementedError main function.

---
+++ mlab-ns-simulator/mlabsim/main.py
@@ -0,0 +1,2 @@
+def main():
+    raise NotImplementedError('mlabsim.main.main')
--- mlab-ns-simulator/setup.py
+++ mlab-ns-simulator/setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-VERSION = '0.0'
+# Note: We follow PEP-0440 versioning:
+# http://legacy.python.org/dev/peps/pep-0440/
+
+VERSION = '0.1.dev0'
 
 
 setup(
+    # Humanish metadata:
     name='mlab-ns-simulator',
     description='A simulator for the mlab-ns service which provides features Ooni needs.',
     version=VERSION,
@@ -14,7 +18,19 @@ setup(
     author_email='consultancy@leastauthority.com',
     license='FIXME',
     url='https://github.com/LeastAuthority/ooni-support',
-    scripts=[],
-    py_modules=[],
-    install_requires=[],
+
+    # Python structure for this package:
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'mlabsim = mlabsim.main:main',
+            ],
+        },
+    test_suite='mlabsim.tests',
+
+    # Dependencies:
+    # Note: The dependency versions are chosen to match ooni-backend where they overlap:
+    install_requires=[
+        'twisted == 13.0',
+        ]
     )

bf01ce3a2f5e728199354a0de463f7ec991f5afe
Taylor Hornby <havoc@defuse.ca>
2014-07-21 16:53:50 -0600

    Simpler file reading

--- bouncer-plumbing/collector-to-mlab/getconfig.py
+++ bouncer-plumbing/collector-to-mlab/getconfig.py
@@ -6,17 +6,17 @@ import yaml
 # TODO: Accept the oonib.conf path on the command line.
 def get_yaml_config_string(oonib_conf='/home/mlab_ooni/oonib.conf'):
     # Open this slice's oonib.conf
-    oonib_conf_contents = StringIO()
-    get(oonib_conf, oonib_conf_contents)
+    # FIXME: Make sure errors are handled correctly, and file is closed.
+    with open(oonib_conf, "r") as f
+        oonib_conf_contents = f.read()
     oonib_conf_parsed = yaml.safe_load(oonib_conf_contents)
 
     # Read this slice's (collector) .onion address.
     tor_datadir = oonib_configuration['main']['tor_datadir']
     tor_hostname_path = os.path.join(tor_datadir, 'collector', 'hostname')
-    tor_hostname_contents = StringIO()
-    get(tor_hostname_path, tor_hostname_contents)
-    tor_hostname_contents.seek(0)
-    tor_onion_address = tor_hostname_contents.readlines()[0].strip()
+    # FIXME: Make sure errors are handled correctly, and file is closed.
+    with open(tor_hostname_path, "r") as f
+        tor_onion_address = f.read().strip()
 
     # Find this slice's IP address.
     slice_ipv4_address = get_ip_address()

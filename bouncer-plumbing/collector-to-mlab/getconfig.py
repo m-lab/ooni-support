09078c0943ae8ae7bdd40b8d76a89e0ab00ea1fe
Taylor Hornby <havoc@defuse.ca>
2014-07-21 17:08:38 -0600

    Comment out non-working code, fix ipv4 function

--- bouncer-plumbing/collector-to-mlab/getconfig.py
+++ bouncer-plumbing/collector-to-mlab/getconfig.py
@@ -2,6 +2,7 @@
 
 import os
 import yaml
+import subprocess
 
 # TODO: Accept the oonib.conf path on the command line.
 def get_yaml_config_string(oonib_conf='/home/mlab_ooni/oonib.conf'):
@@ -11,12 +12,13 @@ def get_yaml_config_string(oonib_conf='/home/mlab_ooni/oonib.conf'):
         oonib_conf_contents = f.read()
     oonib_conf_parsed = yaml.safe_load(oonib_conf_contents)
 
-    # Read this slice's (collector) .onion address.
-    tor_datadir = oonib_conf_parsed['main']['tor_datadir']
-    tor_hostname_path = os.path.join(tor_datadir, 'collector', 'hostname')
-    # FIXME: Make sure errors are handled correctly, and file is closed.
-    with open(tor_hostname_path, "r") as f:
-        tor_onion_address = f.read().strip()
+    # FIXME: Currently the Ooni slices aren't running Tor...???
+    # # Read this slice's (collector) .onion address.
+    # tor_datadir = oonib_conf_parsed['main']['tor_datadir']
+    # tor_hostname_path = os.path.join(tor_datadir, 'collector', 'hostname')
+    # # FIXME: Make sure errors are handled correctly, and file is closed.
+    # with open(tor_hostname_path, "r") as f:
+    #     tor_onion_address = f.read().strip()
 
     # Find this slice's IP address.
     slice_ipv4_address = get_ip_address()
@@ -26,7 +28,7 @@ def get_yaml_config_string(oonib_conf='/home/mlab_ooni/oonib.conf'):
     print tor_onion_address
 
 def get_ip_address():
-    output = Popen(["./get_ipv4.sh"], stdout=PIPE).communicate()[0]
+    output = subprocess.Popen(["./get_ipv4.sh"], stdout=subprocess.PIPE).communicate()[0]
     return output.strip()
 
 def printconfig():

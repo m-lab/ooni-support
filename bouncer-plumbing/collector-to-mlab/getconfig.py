f32493b1d3c7bd79e17f03a20bc56944fa0f4e95
Taylor Hornby <havoc@defuse.ca>
2014-07-21 16:54:58 -0600

    Fix mistakes

--- bouncer-plumbing/collector-to-mlab/getconfig.py
+++ bouncer-plumbing/collector-to-mlab/getconfig.py
@@ -7,15 +7,15 @@ import yaml
 def get_yaml_config_string(oonib_conf='/home/mlab_ooni/oonib.conf'):
     # Open this slice's oonib.conf
     # FIXME: Make sure errors are handled correctly, and file is closed.
-    with open(oonib_conf, "r") as f
+    with open(oonib_conf, "r") as f:
         oonib_conf_contents = f.read()
     oonib_conf_parsed = yaml.safe_load(oonib_conf_contents)
 
     # Read this slice's (collector) .onion address.
-    tor_datadir = oonib_configuration['main']['tor_datadir']
+    tor_datadir = oonib_conf_parsed['main']['tor_datadir']
     tor_hostname_path = os.path.join(tor_datadir, 'collector', 'hostname')
     # FIXME: Make sure errors are handled correctly, and file is closed.
-    with open(tor_hostname_path, "r") as f
+    with open(tor_hostname_path, "r") as f:
         tor_onion_address = f.read().strip()
 
     # Find this slice's IP address.

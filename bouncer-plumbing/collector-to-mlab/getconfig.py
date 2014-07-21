2b5353e32c706410866d6dfb98f9bc90d3256027
Taylor Hornby <havoc@defuse.ca>
2014-07-21 17:57:51 -0600

    Re-enable .onion loading and put information in YAML format

--- bouncer-plumbing/collector-to-mlab/getconfig.py
+++ bouncer-plumbing/collector-to-mlab/getconfig.py
@@ -12,20 +12,26 @@ def get_yaml_config_string(oonib_conf='/home/mlab_ooni/oonib.conf'):
         oonib_conf_contents = f.read()
     oonib_conf_parsed = yaml.safe_load(oonib_conf_contents)
 
-    # FIXME: Currently the Ooni slices aren't running Tor...???
-    # # Read this slice's (collector) .onion address.
-    # tor_datadir = oonib_conf_parsed['main']['tor_datadir']
-    # tor_hostname_path = os.path.join(tor_datadir, 'collector', 'hostname')
-    # # FIXME: Make sure errors are handled correctly, and file is closed.
-    # with open(tor_hostname_path, "r") as f:
-    #     tor_onion_address = f.read().strip()
+    # Read this slice's (collector) .onion address.
+    tor_datadir = oonib_conf_parsed['main']['tor_datadir']
+    tor_hostname_path = os.path.join(tor_datadir, 'collector', 'hostname')
+    # FIXME: Make sure errors are handled correctly, and file is closed.
+    with open(tor_hostname_path, "r") as f:
+        tor_onion_address = f.read().strip()
 
     # Find this slice's IP address.
     slice_ipv4_address = get_ip_address()
 
-    # TESTING: Print what we have so far:
-    print slice_ipv4_address
-    print tor_onion_address
+    # FIXME: Fill with the actual test helper addresses.
+    test_helpers = {}
+
+    config_part = {
+        tor_onion_address: {
+            'test-helpers': test_helpers
+        }
+    }
+
+    return yaml.dump(config_part)
 
 def get_ip_address():
     output = subprocess.Popen(["./get_ipv4.sh"], stdout=subprocess.PIPE).communicate()[0]

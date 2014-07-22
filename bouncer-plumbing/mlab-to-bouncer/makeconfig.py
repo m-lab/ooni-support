9dcf85649b68494a5ea8ebdbfdd7ca7d4438d6ce
Taylor Hornby <havoc@defuse.ca>
2014-07-21 18:24:04 -0600

    Add outline of makeconfig.py

--- bouncer-plumbing/mlab-to-bouncer/makeconfig.py
+++ bouncer-plumbing/mlab-to-bouncer/makeconfig.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+def assemble_bouncer_config(parts):
+    pass
+
+def read_parts_from_stdin():
+    pass
+
+parts = read_parts_from_stdin()
+bouncer_config = assemble_bouncer_config(parts)
+write_bouncer_config(bouncer_config, '/home/mlab/data/bouncer.yaml')

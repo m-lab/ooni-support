42baf59e005c821703ea91f358b469eefe979810
Nathan Wilcox <nejucomo@gmail.com>
2014-07-21 20:07:47 -0700

    Update the test specs to require many parameters taken from the live mlab-ns.

--- mlab-ns-simulator/mlabsim/tests/test_update.py
+++ mlab-ns-simulator/mlabsim/tests/test_update.py
@@ -7,6 +7,17 @@ from mock import MagicMock, call
 
 from mlabsim import update
 
+ExampleLookup = """
+Example response of production: http://mlab-ns.appspot.com/npad?format=json
+
+{'city': 'Mountain View',
+ 'country': 'US',
+ 'fqdn': 'npad.iupui.mlab3.nuq02.measurement-lab.org',
+ 'ip': ['149.20.5.102', '2001:4F8:1:1001::102'],
+ 'port': '8001',
+ 'site': 'nuq02',
+ 'url': 'http://npad.iupui.mlab3.nuq02.measurement-lab.org:8000'}
+"""
 
 class UpdateResourceTests (unittest.TestCase):
 
@@ -17,17 +28,27 @@ class UpdateResourceTests (unittest.TestCase):
         tool_extra = {
             'collector_onion': 'testfakenotreal.onion',
             }
-        tool_extra_param = json.dumps(tool_extra)
+
+        expectedentry = {
+            'fqdn': fqdn,
+            'city': 'Somewheresville',
+            'country': 'US',
+            'ip': ['127.2.3.4', '::1'],
+            'port': 8421,
+            'site': 'mlab01',
+            'tool_extra': tool_extra
+            }
 
         # Mocks:
         db = {}
         m_request = MagicMock()
 
         # Fake a request with sufficient parameters:
-        m_request.args = {
-            'fqdn': [fqdn],
-            'tool_extra': [tool_extra_param],
-            }
+        # The args are just expectedentry with each value in a single-item list:
+        m_request.args = dict( (k, [v]) for (k, v) in expectedentry.iteritems() )
+
+        # Except tool_extra is json encoded:
+        m_request.args['tool_extra'] = [json.dumps(tool_extra)]
 
         # Execute the code under test:
         ur = update.UpdateResource(db)
@@ -37,7 +58,7 @@ class UpdateResourceTests (unittest.TestCase):
         self.assertEqual(server.NOT_DONE_YET, retval)
 
         # Verify that m_db now stores fqdn: tool_extra:
-        self.assertEqual({fqdn: {"tool_extra": tool_extra}}, db)
+        self.assertEqual({fqdn: expectedentry}, db)
 
         # Verify that a 200 response was sent:
         self.assertEqual(
@@ -53,18 +74,28 @@ class UpdateResourceTests (unittest.TestCase):
         tool_extra = {
             'collector_onion': 'testfakenotreal.onion',
             }
-        # The slice, [1:] is to mangle the json:
-        tool_extra_param = json.dumps(tool_extra)[1:]
+
+        expectedentry = {
+            'fqdn': fqdn,
+            'city': 'Somewheresville',
+            'country': 'US',
+            'ip': ['127.2.3.4', '::1'],
+            'port': 8421,
+            'site': 'mlab01',
+            'tool_extra': tool_extra
+            }
 
         # Mocks / components:
         m_db = MagicMock()
         m_request = MagicMock()
 
         # Fake a request with sufficient parameters:
-        m_request.args = {
-            'fqdn': [fqdn],
-            'tool_extra': [tool_extra_param],
-            }
+        # The args are just expectedentry with each value in a single-item list:
+        m_request.args = dict( (k, [v]) for (k, v) in expectedentry.iteritems() )
+
+        # Except tool_extra is json encoded then mangled with a slice:
+        m_request.args['tool_extra'] = [json.dumps(tool_extra)[:-1]]
+
 
         # Execute the code under test:
         ur = update.UpdateResource(m_db)

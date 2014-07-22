7aaaaee3e02ab7ff41a3580ca96325377a1f4d1d
Nathan Wilcox <nejucomo@gmail.com>
2014-07-21 19:20:00 -0700

    Add a new test for invalid JSON and update the old test to verify mock usage with ``.mock_calls``.

--- mlab-ns-simulator/mlabsim/tests/test_update.py
+++ mlab-ns-simulator/mlabsim/tests/test_update.py
@@ -2,8 +2,9 @@ import json
 import urllib
 
 from twisted.trial import unittest
+from twisted.web import server
 
-import mock
+from mock import MagicMock, call
 
 from mlabsim import update
 
@@ -19,11 +20,9 @@ class UpdateResourceTests (unittest.TestCase):
             }
         tool_extra_param = urllib.quote(json.dumps(tool_extra))
 
-        # Mocks / components:
-        db = {}
-
         # Mocks:
-        m_request = mock.MagicMock()
+        db = {}
+        m_request = MagicMock()
 
         # Fake a request with sufficient parameters:
         m_request.params = {
@@ -33,10 +32,57 @@ class UpdateResourceTests (unittest.TestCase):
 
         # Execute the code under test:
         ur = update.UpdateResource(db)
-        ur.render_PUT(m_request)
+        retval = ur.render_PUT(m_request)
+
+        # Verifications:
+        self.assertEqual(server.NOT_DONE_YET, retval)
 
         # Verify that m_db now stores fqdn: tool_extra:
         self.assertEqual({fqdn: {"tool_extra": tool_extra}}, db)
 
+        # Verify that a 200 response was sent:
+        self.assertEqual(
+            m_request.mock_calls,
+            [call.setResponseCode(200, 'ok'),
+             call.finish(),
+             ])
 
+    def test_render_PUT_malformed_JSON(self):
+        # Test data:
+        fqdn = 'mlab01.ooni-tests.not-real.except-it-actually-could-be.example.com'
 
+        tool_extra = {
+            'collector_onion': 'testfakenotreal.onion',
+            }
+        # The slice, [1:] is to mangle the json:
+        tool_extra_param = urllib.quote(json.dumps(tool_extra)[1:])
+
+        # Mocks / components:
+        m_db = MagicMock()
+        m_request = MagicMock()
+
+        # Fake a request with sufficient parameters:
+        m_request.params = {
+            'fqdn': fqdn,
+            'tool_extra': tool_extra_param,
+            }
+
+        # Execute the code under test:
+        ur = update.UpdateResource(m_db)
+        retval = ur.render_PUT(m_request)
+
+        # Verifications:
+        self.assertEqual(server.NOT_DONE_YET, retval)
+
+        # Verify that m_db was not modified (or accessed) in any way:
+        self.assertEqual(
+            m_db.mock_calls,
+            [],
+            )
+
+        # Verify that a 400 response was sent:
+        self.assertEqual(
+            m_request.mock_calls,
+            [call.setResponseCode(400, 'invalid'),
+             call.finish(),
+             ])

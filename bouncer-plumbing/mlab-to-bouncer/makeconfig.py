96eaef0f4b4ba25b09659c0aa030201942f1a3e9
Taylor Hornby <havoc@defuse.ca>
2014-07-23 12:23:17 -0600

    Finish the bouncer.yaml re-combination

--- bouncer-plumbing/mlab-to-bouncer/makeconfig.py
+++ bouncer-plumbing/mlab-to-bouncer/makeconfig.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
-def assemble_bouncer_config(parts):
-    pass
+import sys
+import yaml
 
 def read_parts_from_stdin():
-    pass
+    data = sys.stdin.read()
+    parts_string = data.split("----")
+    parts_parsed = []
+    for part in parts_string:
+        part_parsed = yaml.safe_load(part)
+        parts_parsed.append(part_parsed)
+    return parts_parsed
+
+def assemble_bouncer_config(parts):
+    merged_parts = { }
+    for part in parts:
+        merged_parts.update(part)
+    bouncer_config = { 'collector': merged_parts }
+    return yaml.dump(bouncer_config)
+
+def write_bouncer_config(bouncer_config, path):
+    print bouncer_config
+
 
 parts = read_parts_from_stdin()
 bouncer_config = assemble_bouncer_config(parts)

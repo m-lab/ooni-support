7b50c9290a8c8d3481d9147ebb66d3b7868ad7fc
Taylor Hornby <havoc@defuse.ca>
2014-07-23 12:38:01 -0600

    Write to bouncer config file

--- bouncer-plumbing/mlab-to-bouncer/makeconfig.py
+++ bouncer-plumbing/mlab-to-bouncer/makeconfig.py
@@ -19,10 +19,21 @@ def assemble_bouncer_config(parts):
     bouncer_config = { 'collector': merged_parts }
     return yaml.dump(bouncer_config)
 
-def write_bouncer_config(bouncer_config, path):
-    print bouncer_config
+def write_bouncer_config(path, bouncer_config_contents):
+    try:
+        f = open(path, 'w')
+        f.write(bouncer_config_contents)
+        f.close()
+    except IOError:
+        print "Couldn't write to bouncer config file."
+        exit(1)
 
 
+bouncer_config_path = '/home/mlab/data/bouncer.yaml'
+if len(sys.argv) >= 2:
+    bouncer_config_path = sys.argv[1]
+
+# FIXME: Read from the mlab-ns simulator.
 parts = read_parts_from_stdin()
 bouncer_config = assemble_bouncer_config(parts)
-write_bouncer_config(bouncer_config, '/home/mlab/data/bouncer.yaml')
+write_bouncer_config(bouncer_config_path, bouncer_config)

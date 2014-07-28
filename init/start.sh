1683b104ed974a4d52c00d55e462e5e1eb09e401
Nathan Wilcox <nejucomo@gmail.com>
2014-07-28 15:10:47 -0700

    Start mlabsim if we're on the magic hostname mlab1.nuq0t.measurement-lab.org.

--- init/start.sh
+++ init/start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+MLABSIM_HOME='mlab1.nuq0t.measurement-lab.org'
+
 source /etc/mlab/slice-functions
 cd $SLICEHOME
 # NOTE: oonib drops privilges if UID/GID are set in oonib.conf
 $SLICEHOME/bin/oonib -c $SLICEHOME/oonib.conf &
+
+if [ "$(hostname)" = "$MLABSIM_HOME" ]; then
+    echo 'Starting mlabsim...'
+    $SLICEHOME/bin/mlabsim --log-level DEBUG &
+fi

fffae54251e7b7aa716de4d1b383aa1e4e8c40fc
Taylor Hornby <havoc@defuse.ca>
2014-07-28 11:33:59 -0600

    Need to be root to restart

--- bouncer-plumbing/mlab-to-bouncer/update-bouncer.sh
+++ bouncer-plumbing/mlab-to-bouncer/update-bouncer.sh
@@ -2,5 +2,5 @@
 # Fetch and install the latest bouncer configuration.
 python2 makeconfig.py
 # Restart the bouncer.
-/home/mlab_ooni/init/stop.sh
-/home/mlab_ooni/init/start.sh
+sudo /home/mlab_ooni/init/stop.sh
+sudo /home/mlab_ooni/init/start.sh

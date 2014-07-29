27dd91420c2306edc43e58a103f1a80c47f1b8e3
Nathan Wilcox <nejucomo@gmail.com>
2014-07-29 14:45:37 -0700

    Add an ENABLED flag inside update-bouncer.sh and self-describe the script's purpose.

--- bouncer-plumbing/mlab-to-bouncer/update-bouncer.sh
+++ bouncer-plumbing/mlab-to-bouncer/update-bouncer.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-# Fetch and install the latest bouncer configuration.
-python2 makeconfig.py
-# Restart the bouncer.
-sudo /home/mlab_ooni/init/stop.sh
-sudo /home/mlab_ooni/init/start.sh
+#
+# This script overwrites the Ooni bouncer configuration file on this
+# host based on information gathered from mlab-ns (or a simulator).
+# It then stops and starts the bouncer.
+#
+# If ENABLED is not 'true' it will skip this, so if you want to disable
+# mlab-ns integration for the Ooni bouncer, set this to 'false':
+
+ENABLED=true
+
+if [ "$ENABLED" = 'true' ]; then
+    # Fetch and install the latest bouncer configuration.
+    makeconfig.py
+
+    # Restart the bouncer.
+    sudo /home/mlab_ooni/init/stop.sh
+    sudo /home/mlab_ooni/init/start.sh
+fi

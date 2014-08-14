54403cd4df6d39ddea6a25c8434b5c3e47cfa482
Taylor Hornby <havoc@defuse.ca>
2014-08-14 09:53:48 -0600

    Print to stdout instead of PUTting

--- bouncer-plumbing/collector-to-mlab/getconfig.py
+++ bouncer-plumbing/collector-to-mlab/getconfig.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 
-# This script builds the portion of the bouncer.yaml config file for the Ooni
+# This script collects the portion of the bouncer.yaml config file for the Ooni
 # collector and test helpers running on an M-Lab slice. To use it, run `python
-# getconfig.py`, and if all goes well, the YAML portion will be printed to
-# stdout. If something goes wrong, an error message will be printed to stdout,
-# and the exit status will be non-zero. The script assumes the oonib.conf file
-# is in /home/mlab_ooni/oonib.conf. If not, pass the path as the first command
-# line option.
+# getconfig.py`, and if all goes well, the configuration portion, encoded in
+# JSON, will be printed to stdout. If something goes wrong, an error message
+# will be printed to stdout, and the exit status will be non-zero. The script
+# assumes the oonib.conf file is in /home/mlab_ooni/oonib.conf. If not, pass the
+# path as the first command-line option.
 
 import sys
 import os
 import yaml
 import json
 import subprocess
-import urllib2
-
-# For a hack
-import time
-
-MLAB_SIMULATOR_URL = "http://127.0.0.1:8585/update-ooni"
 
 def get_bouncer_config_part(oonib_conf):
     try:
@@ -47,17 +41,15 @@ def get_bouncer_config_part(oonib_conf):
 
     # List the running test helpers and their addresses.
     test_helpers = {}
-    # FIXME: This should be a dynamically-generated list of all the test helpers
-    # that are actually running. However, I have no idea how to infer which ones
-    # are running and which ones aren't from the oonib.conf, since it seems to
-    # be the same regardless of whether they're running or not (is there some
-    # other source of information?)
     try:
         # For this first M-Lab deployment, we only support one test, which is
         # http-return-json-headers. In the future, this script should
         # automatically determine which helpers are running on a slice and
         # include exactly those. This is tracked in:
         # https://github.com/m-lab-tools/ooni-support/issues/55
+
+        # Test helpers are disabled by setting the port to Null in the YAML,
+        # which translates to None in Python.
 
         http_return_headers_port = oonib_conf_parsed['helpers']['http-return-json-headers']['port']
         if http_return_headers_port is not None:
@@ -98,29 +90,12 @@ def return_failure(msg):
     print "ERROR: " + msg
     exit(1)
 
-def put_config(oonib_conf):
+def print_config(oonib_conf):
     part = get_bouncer_config_part(oonib_conf)
-    put_parameters = {
-        'city': 'foobar',
-        'country': 'foobar',
-        'fqdn': "nothing.google.com" + str(time.time()),
-        'ip': '127.0.0.1',
-        'port': '0',
-        'site': 'mars',
-        'tool_extra': part
-    }
-    send_put(json.dumps(put_parameters))
-    
-def send_put(json_body):
-    # https://stackoverflow.com/questions/111945/is-there-any-way-to-do-http-put-in-python
-    opener = urllib2.build_opener(urllib2.HTTPHandler)
-    request = urllib2.Request(MLAB_SIMULATOR_URL, data=json_body)
-    request.add_header('Content-Type', 'application/json')
-    request.get_method = lambda: 'PUT'
-    opener.open(request)
+    print json.dumps(part)
 
 oonib_conf = '/home/mlab_ooni/oonib.conf'
 if len(sys.argv) >= 2:
     oonib_conf = sys.argv[1]
 
-put_config(oonib_conf)
+print_config(oonib_conf)
